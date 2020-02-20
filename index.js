a6aca307f05b4725cb60ff86dd7981f7c45f7c2e
PetarAsenov <p.a.asenov@gmail.com>
2020-02-20 09:40:57 +0100

    index creation

---
+++ index.js
@@ -0,0 +1,4 @@
+const toTitleCase = require("./node_modules/to-title-case/index")
+const lower = 'the lord of the rings: the return of the king'
+const title = toTitleCase(lower)
+console.log(title)
